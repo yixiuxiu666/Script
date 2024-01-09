98c4e0a3459eaffb265037b8e4b8f5aaeb9203a0
yixiuxiu666 <54693844+yixiuxiu666@users.noreply.github.com>
2024-01-09 11:36:05 +0800

    Update imaotai.js

--- imaotai.js
+++ imaotai.js
@@ -12,11 +12,11 @@
 hostname = app.moutai519.com.cn
 
 [rewrite_local]
-https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js
+https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/yixiuxiu666/Script/main/imaotai.js
 
 [task_local]
 # 茅台自动预约
-0,30 9-20 * * * https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
+0,30 9-20 * * * https://raw.githubusercontent.com/yixiuxiu666/Script/main/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
 ```
 ******************************************/
 const $ = new Env('i茅台'), service = $.http
@@ -29,10 +29,10 @@ var province = $.getdata('imaotai__config__province') || '' // 省份
 var city = $.getdata('imaotai__config__city') || '' // 城市
 var itemList = $.getdata('imaotai__config__itemcode')?.split(',') || ['10213', '10214'] // 预约项
 var itemMap = {
-    10213: '贵州茅台酒（癸卯兔年）',
+    10941: '贵州茅台酒（甲辰龙年）',
     10056: '53%vol 500ml 茅台1935',
     2478: '贵州茅台酒（珍品）',
-    10214: '贵州茅台酒（癸卯兔年）x2'
+    10942: '贵州茅台酒（甲辰龙年）x2'
 }
 var address = $.getdata('imaotai__config__address') || '' // 详细地址
 var location = $.getdata('imaotai__config__location') || '' // 地址经纬度
